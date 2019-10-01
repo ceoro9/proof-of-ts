7add91726fb42d0fa9dd109b232e7d7fb39596b3
Roman Aleksandrovich <ceoro9@protonmail.com>
2019-10-01 23:19:14 +0300

    Remove boilerplate code from schema validation

--- userService/src/business-layer/validators/BaseValidationSchema.ts
+++ userService/src/business-layer/validators/BaseValidationSchema.ts
@@ -1,6 +1,32 @@
 import { validate as validateSchema } from 'class-validator';
+import { plainToClass }               from 'class-transformer';
+import { ClassType }                  from 'class-transformer/ClassTransformer';
 
-export default class BaseValidationSchema {
+export default abstract class BaseValidationSchema {
+
+	/**
+	 * Transforms dato to instance of schema class
+	 * and merge it with current constructing object.
+	 * @param data object to validate
+	 */	
+	public constructor(data: object) {
+		
+		// Obtain class-constructor of current schema
+		// (it can be only sub-classes of BaseValidationSchema)
+		const SchemaClass = (
+      <{ [Symbol.species]: ClassType<BaseValidationSchema> }><unknown>this.constructor
+    )[Symbol.species];
+		
+		// Transform arbitary owbject to instance of this schema class 
+		const schemaInstance = plainToClass(SchemaClass, data);
+		
+		// Merge current constructing instance of schema with transformed one
+		Object.assign(this, schemaInstance);
+	}
+
+	protected static get [Symbol.species]() {
+		return this;
+	}
 
 	public async validate() {
 		const validationErrors = await validateSchema(this);
@@ -10,3 +36,5 @@ export default class BaseValidationSchema {
 	}
 
 }
+
+
--- userService/src/business-layer/validators/UserValidationSchema.ts
+++ userService/src/business-layer/validators/UserValidationSchema.ts
@@ -6,30 +6,20 @@ import { Gender } from '@app/data-layer/constants';
 export class UserRegisterRequestValidationSchema extends BaseValidationSchema implements UserRegisterRequest {
 
 	@Length(2, 100)
-	public firstName: string;
+	public firstName!: string;
 
 	@Length(2, 200)
-	public lastName: string;
+	public lastName!: string;
 
 	@MinLength(2, { message: 'Username is too short' })
 	@MaxLength(80, { message: 'Username is too long' })
-	public username: string;
+	public username!: string;
 
 	@IsEmail()
-	public email: string;
+	public email!: string;
 
 	@IsEnum(Gender)
-	public gender: string;
-
-	public constructor(request: UserRegisterRequest) {
-		super();
-		// TODO: find a way to avoid such boilerplate code
-		this.firstName = request.firstName;
-		this.lastName  = request.lastName;
-		this.username  = request.username;
-		this.email     = request.email;
-		this.gender    = request.gender;
-	}
+	public gender!: string;
 
 }
 
@@ -46,13 +36,5 @@ export class UserPatchRequestValidationSchema extends BaseValidationSchema imple
 	@IsOptional()
 	@IsEnum(Gender)
 	public gender?: string;
-
-	public constructor(request: UserPatchRequest) {
-		super();
-		// TODO: find a way to avoid such boilerplate code
-		this.firstName = request.firstName;
-		this.lastName  = request.lastName;
-		this.gender    = request.gender;
-	}
 
 }
