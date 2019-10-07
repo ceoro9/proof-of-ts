1d1d37c27ca0bc3f8a05ad5e96a4ee3a5a446c48
Roman Aleksandrovich <ceoro9@protonmail.com>
2019-10-07 17:35:35 +0300

    Configure dynamic config module

--- postService/src/config/config.module.ts
+++ postService/src/config/config.module.ts
@@ -1,23 +1,21 @@
-import { Module, DynamicModule } from '@nestjs/common';
+import { Module, DynamicModule, Provider } from '@nestjs/common';
 import { ConfigService } from './config.service';
 
-@Module({
-	exports:   [ConfigService],
-	providers: [ConfigService],
-})
+@Module({})	
 export class ConfigModule {
 
 	public static register(configObject?: any): DynamicModule {
+
+		const configService: Provider<ConfigService> = {
+			provide:  ConfigService,
+			useValue: new ConfigService(configObject),
+		};
+
 		return {
-			module: this,
-			providers: [
-				{
-					provide:  ConfigService,
-					useValue: new ConfigService(configObject),
-				}
-			],
-			exports: [ConfigService],
-		}
+			module:    ConfigModule,
+			providers: [configService],
+			exports:   [configService],
+		};
 	}
 
 }
--- postService/src/config/config.service.ts
+++ postService/src/config/config.service.ts
@@ -11,20 +11,29 @@ export class ConfigService {
 	public constructor(filePath: string);
 	public constructor(configObject: { [key: string]: string | undefined });
 	public constructor(obj?: string | { [key: string]: string | undefined }) {
-
-		if (typeof obj === "string") {
-			this.envConfig = dotenv.parse(fs.readFileSync(obj));
-		}
 		
-		if (typeof obj === "object") {
-			this.envConfig = obj;
-		}
+		switch (typeof obj) {
 
-		if (typeof obj === "undefined") {
-			this.envConfig = process.env;
-		}
+			case "string": {
+				this.envConfig = dotenv.parse(fs.readFileSync(obj));				
+				break;
+			}
 
-		throw new Error(`Invalid configuration parameter: ${obj}`);
+			case "object": {
+				this.envConfig = obj;
+				break;
+			}
+
+			case "undefined": {
+				this.envConfig = process.env;
+				break;
+			}
+
+			default: {
+				throw new Error(`Invalid configuration parameter: ${obj}`);
+			}
+		}
+	
 	}
 
 	public get(key: string) {
