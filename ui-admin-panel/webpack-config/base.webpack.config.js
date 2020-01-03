dd6ae94890af3a2391cc595b0ace7679d2f7bfa3
Roman Aleksandrovich <ceoro9@protonmail.com>
2020-01-03 15:39:34 +0300

    Set up react and its build process

--- ui-admin-panel/webpack-config/base.webpack.config.js
+++ ui-admin-panel/webpack-config/base.webpack.config.js
@@ -1,35 +1,80 @@
-const path = require('path');
+const path                       = require('path');
+const HtmlWebpackPlugin          = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const projectRootPath            = path.resolve(__dirname, '../');
+const nodeModulesFolderPath      = path.resolve(projectRootPath, 'node_modules/');
+const srcFolderPath              = path.resolve(projectRootPath, 'src/');
+const distFolderPath             = path.resolve(projectRootPath, 'dist/');
 
 module.exports = {
-	entry: "./src/",
-
+  entry: srcFolderPath,
+  
 	output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: distFolderPath,
 		publicPath: "/"
-	},
-
+  },
+  
 	module: {
 		rules: [
 			{
 				test: /\.tsx?$/,
 				include: [
-		    	path.resolve(__dirname, "src")
-				],
-				loader: "ts-loader"
+		    	srcFolderPath
+        ],
+        exclude: [
+          nodeModulesFolderPath
+        ],
+        use: [
+          {
+            loader: 'cache-loader'
+          },
+          {
+            loader: 'thread-loader',
+            options: {
+              // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+              workers: require('os').cpus().length - 1,
+              // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
+              // poolTimeout: Infinity
+            }
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(projectRootPath, 'tsconfig.json'),
+              // disable type-checking(it handles on separate process by fork plugin)
+              transpileOnly: true,
+              // parallise building process
+              happyPackMode: true
+            }
+          }
+        ]        
 	  	}
 		]
   },
 
   resolve: {
 		modules: [
-	    "node_modules",
-	    path.resolve(__dirname, "src")
+	    nodeModulesFolderPath,
+	    srcFolderPath
 		],
 		extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
   },
 
   performance: {
 		hints: "warning"
-  }
+  },
+
+  plugins: [
+    // Generates html page with a resulting bundle
+    new HtmlWebpackPlugin({
+      template: path.resolve(srcFolderPath, "index.html")
+    }),
+    // Runs Typescript type-checker on a separate process 
+    new ForkTsCheckerWebpackPlugin({
+      useTypescriptIncrementalApi: true,
+      checkSyntacticErrors: true,
+    }),
+  ]
 };
 
--- ui-admin-panel/webpack-config/dev.webpack.config.js
+++ ui-admin-panel/webpack-config/dev.webpack.config.js
@@ -1,9 +1,18 @@
+const path              = require('path');
 const baseWebpackConfig = require('./base.webpack.config');
+
+const projectRootPath   = path.resolve(__dirname, '../');
+const srcFolderPath     = path.resolve(projectRootPath, 'src/');
 
 module.exports = {
   ...baseWebpackConfig,
   mode: "development",
-
+  devServer: {
+    contentBase: path.join(srcFolderPath, 'dist'),
+    compress: true,
+    hot: true,
+    port: process.env.UI_ADMIN_PANEL_DEV_SERVER_PORT || 9000
+  },
   output: {
     ...baseWebpackConfig.output,
     filename: "dev.bundle.js"
